f7d4fb94189bdad8aeaf98518b016502142b1d35
ominous-signal <91231558+ominous-signal@users.noreply.github.com>
2022-09-23 13:41:34 +1000

    Update GPSInject-GetResults.py

--- jetson_code/GPSInject-GetResults.py
+++ jetson_code/GPSInject-GetResults.py
@@ -31,6 +31,9 @@ injection = {'lat':-35.3599712, 'lon':149.1542315}
 connString = "tcp:127.0.0.1:5760"
 
 the_connection = mavutil.mavlink_connection(connString)
+
+the_connection.wait_heartbeat()
+
 print("Heartbeat from system (system %u component %u)" % (the_connection.target_system, the_connection.target_component))
 
 
