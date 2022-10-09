b6753fa7cce297913bd4992b9f98c87510adc0d3
ClancyWalters <103063433@student.swin.edu.au>
2022-10-09 14:57:57 +1100

    thrusday work

--- other_scripts/kml_generation.sh
+++ other_scripts/kml_generation.sh
@@ -1,2 +1,2 @@
 #This script is nolonger needed, it only runs a single command
-python kml_generation.py --radius 4 --altitude 150 --verbose --precompute --duration 6 --delimiter --path myplaces.kml
+python kml_generation.py --radius 3 --altitude 200 --verbose --precompute --duration 6 --delimiter --path myplaces.kml
